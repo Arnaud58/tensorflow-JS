0d28114ffb441573437d271944eb8ca703cc7a11
Rougetet Arnaud <arnaud.rougetet.etu@univ-lille.fr>
2019-01-29 11:14:42 +0100

    Essaie sur tensorFlow

--- web/sketch.js
+++ web/sketch.js
@@ -6,6 +6,7 @@ const learningRate = 0.5;
 const optimizer = tf.train.sgd(learningRate);
 
 let model;
+history;
 
 /*
 Création d'un premier réseau neuronal
@@ -13,30 +14,30 @@ Couche d'entrée : 2 neurones (hauteur et largeur)
 1 couche cachée : 3 neurones
 Couche de sortie : 2 neurones ("Haut" et "Bas")
 */
-function createNeuralNetwork(){
+function createNeuralNetwork() {
     model = tf.sequential();
 
     const hiddenConfig = {
-        inputShape : [2],
+        inputShape: [2],
         units: 3,
         activation: 'relu'
     };
     const outputConfig = {
-      units: 1,
-      activation: 'softmax'
+        units: 1,
+        activation: 'softmax'
     };
     let hiddenLayer = tf.layers.dense(hiddenConfig);
     let outputLayer = tf.layers.dense(outputConfig);
     model.add(hiddenLayer);
     model.add(outputLayer);
-    
-    model.compil({
-      optimizer: optimizer,
-      loss: 'meanSquaerError'
+
+    model.compile({
+        optimizer: optimizer,
+        loss: 'meanSquaredError'
     });
-
-
 }
+
+
 
 function addSquare() {
     // Calcul une hauteur et une largeur random
@@ -49,22 +50,27 @@ function addSquare() {
     // Si grand rectangle, va en haut, sinon va en bas
     if (hauteur * largeur > 30000) {
         all_learn_squares["pos"].push("Haut");
+
+        history = model.fit([tf.tensor([
+            [largeur, hauteur]
+        ])], tf.tensor([1]));
     } else {
         all_learn_squares["pos"].push("Bas");
-    }
 
+        history = model.fit([tf.tensor([
+            [largeur, hauteur]
+        ])], tf.tensor([0]));
+    }
     // Lui choisis une couleur random (pour affichage)
     all_learn_squares["color"].push({ r: random(255), g: random(255), b: random(255) });
-}
 
-function autoAdd() {
-
+    // model.predict(tf.variable(tf.tensor([[80,10]]))).print()
 }
 
 function setup() {
     // 2 rectangle de 600*800 avec un gap de 100 entre les 2
     createCanvas(1300, 800);
-    frameRate(1);
+    // frameRate(1);  // Change les fps à 1 images par secondes
 
     button = createButton('Add one learn square');
     button.mousePressed(addSquare);
@@ -74,9 +80,13 @@ function setup() {
 
     buttonAutoAdd = createButton('Ajout auto : désactivé');
     buttonAutoAdd.mousePressed(function() { autoAjout = !autoAjout; if (autoAjout) { buttonAutoAdd.elt.innerText = "Ajout auto : activé"; } else { buttonAutoAdd.elt.innerText = "Ajout auto : désactivé"; } });
+
+    createNeuralNetwork();
 }
 
-
+/*
+ * Fonction appelé par p5 à chaque frame
+ */
 function draw() {
     // Arrière plan
     background(255);
@@ -93,7 +103,7 @@ function draw() {
     stroke('#222222');
     rect(600, 0, 100, 800);
 
-    // Draw all rectangle for the test
+    // Déssine chaque rectangle d'entrainement de all_learn_squares
     for (i = 0; i < all_learn_squares["squareCoord"].length; i++) {
         xGap = (i % 50) * 5;
         yGap = (int(i / 50) * 20) + 20;
@@ -107,6 +117,7 @@ function draw() {
         rect(xGap, yGap, all_learn_squares.squareCoord[i].l, all_learn_squares.squareCoord[i].h);
     }
 
+    // Si activé par le bouton, rajoute un nouveau rectangle d'entrainement
     if (autoAjout) {
         addSquare();
     }
