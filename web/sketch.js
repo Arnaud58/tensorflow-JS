8279c75c13bb8a3ba85ef808e0635e6a9afddf10
Rougetet Arnaud <arnaud.rougetet.etu@univ-lille.fr>
2019-02-06 13:52:36 +0100

    Modif mineurs

--- web/sketch.js
+++ web/sketch.js
@@ -41,7 +41,7 @@ function createNeuralNetwork() {
     });
 }
 
-function download(content, fileName, contentType) {
+function download(content, fileName, contentType = "application/json") {
     var a = document.createElement("a");
     var file = new Blob([content], { type: contentType });
     a.href = URL.createObjectURL(file);
