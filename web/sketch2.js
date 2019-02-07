116679db6619d6e3e9851152ce00e8c3c700d960
Beauprez Ellie <ellie.beauprez.etu@univ-lille.fr>
2019-02-07 10:10:13 +0100

    sketch2.js opérationnel, fait les calculs une fois puis affiche tout, au lieu de les refaire à chaque fois que draw() => optimisation des performances. Il faut maintenant ajuster le réseau neuronal.

--- web/sketch2.js
+++ web/sketch2.js
@@ -1,9 +1,19 @@
 let dimensions = [];
 let positions = [];
 
+let all_learn_squares = { squareCoord: [], pos: [], color: [] };
+let all_predict_squares = { squareCoord: [], pos: [], color: [] };
+
 const learningRate = 0.5;
 const optimizer = 'sgd';
 let model;
+
+//Là où on stocke les positions obtenues en résultat
+let res;
+
+//les tensors utilisés pour l'apprentissage
+let x_train;
+let y_train;
 
 function randomInt(max){
   return Math.floor(Math.random() * Math.floor(max));
@@ -47,9 +57,7 @@ function createNeuralNetwork(){
 }
 
 
-
 async function predictOutput(){
-
   createNeuralNetwork();
   console.log("Neural network created");
 
@@ -57,17 +65,98 @@ async function predictOutput(){
   xs.print();
 
   await model.fit(x_train,y_train, {batchsize:1, epochs:1});
-  let ys = model.predict(xs);
+  ys = model.predict(xs);
+  console.log("Résultat :");
   ys.print();
+  res = Array.from(ys.dataSync());
+  setPredictSquares();
+}
+
+function setLearnSquares(){
+  for (var i=0; i<dimensions.length; i++){
+    all_learn_squares["squareCoord"].push({ l: dimensions[i][1], h: dimensions[i][0] });
+    all_learn_squares["pos"].push(positions[i]);
+    all_learn_squares["color"].push({ r: random(255), g: random(255), b: random(255) });
+  }
+}
+
+function setPredictSquares(){
+    for (var i=0; i<res.length; i++){
+      all_predict_squares["squareCoord"].push({ l: dimensions[i][1], h: dimensions[i][0] });
+      all_predict_squares["pos"].push(res[i]);
+
+      if (isCorrect(positions[i], res[i])){
+        all_predict_squares["color"].push({ r: 0, g: 255, b: 0 });
+      }
+      else{
+        all_predict_squares["color"].push({ r: 255, g: 0, b: 0 });
+      }
+    }
 }
 
 
-generateDataSet();
-let x_train = tf.tensor2d(dimensions);
-console.log("x_train");
-x_train.print();
-let y_train = tf.tensor2d(positions);
-console.log("y_train");
-y_train.print();
+function isCorrect(pos, predict_pos){
+  return (pos==0 && predict_pos<0.5) || (pos==1 && predict_pos>0.5);
+}
 
-predictOutput();
+function setup(){
+  createCanvas(1300, 800);
+  frameRate(1);
+  generateDataSet();
+  setLearnSquares();
+  x_train = tf.tensor2d(dimensions);
+  console.log("x_train");
+  x_train.print();
+  y_train = tf.tensor2d(positions);
+  console.log("y_train");
+  y_train.print();
+  predictOutput();
+
+}
+
+
+function draw() {
+    // Arrière plan
+    background(255);
+
+    // Draw le contours des 2 rectangles
+    noFill();
+    strokeWeight(4);
+    stroke('#222222');
+    rect(0, 0, 1300, 800);
+
+    // Draw le gap entre les deux
+    fill(0);
+    strokeWeight(2);
+    stroke('#222222');
+    rect(600, 0, 100, 800);
+
+    // Draw all learning rectangles
+    for (i = 0; i < all_learn_squares["squareCoord"].length; i++) {
+        xGap = (i % 50) * 5;
+        yGap = (int(i / 50) * 20) + 20;
+
+        if (all_learn_squares.pos[i] == "0") {
+            yGap += 400;
+        }
+      fill(all_learn_squares.color[i].r, all_learn_squares.color[i].g, all_learn_squares.color[i].b);
+      rect(xGap, yGap, all_learn_squares.squareCoord[i].l, all_learn_squares.squareCoord[i].h);
+    }
+
+    // Draw all the predicted rectangles
+    for (i = 0; i < res.length; i++) {
+        xGap = 700 + (i % 50) * 5;
+        yGap = (int(i / 50) * 20) + 20;
+
+        if (all_predict_squares.pos[i]<0.5) {
+            yGap += 400;
+        }
+
+        fill(all_predict_squares.color[i].r, all_predict_squares.color[i].g, all_predict_squares.color[i].b);
+        rect(xGap, yGap, all_predict_squares.squareCoord[i].l, all_predict_squares.squareCoord[i].h);
+    }
+
+
+
+
+}
