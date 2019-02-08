bb9aebf7d8c75e1bc35d35bff1de347bc15cf0c3
Beauprez Ellie <ellie.beauprez.etu@univ-lille.fr>
2019-02-08 13:48:44 +0100

    màj

--- readme.md
+++ readme.md
@@ -5,11 +5,17 @@ Utiliser une seule couche de neurones. <br/>
 Construire mini réseau de neurone avec 2 jeux, un jeu d'apprentisage et un jeu de test. <br/>
 Etoffer le réseau au fur et à mesure avec un nombre de couches et un nombre de neurones différents.
 
+![](assets/d1.png)
+
 Fonctions d'activation à tester :
  - Relu
  - pRelu
  - leakyRelu
  - softmax ou sigmoid pour la couche de sortie
+ 
+ 
+![](assets/d2.png)
+
 
  Dans un premier temps, on prend en compte la grosseur des classes (taille des rectangles) mais on pourra aussi compliquer la chose par la suite avec plus de paramètres d'entrée et de sortie. <br/>
 => Récupérer ces données à l'aide de fichiers JSON <br/>
@@ -17,17 +23,28 @@ Fonctions d'activation à tester :
 
 Ce qui a été réalisé
 ====================
-(dans branche ellie) <br/>
-Un jeu de données est créé (aléatoirement pour l'instant). Ce jeu de données contient une série de rectangles définis par leurs dimensions et leur position. <br/>
-Le réseau neuronal fait l'apprentissage à partir de ces données et fait une prédiction de la position à partir des dimensions des rectangles.
-Pour l'affichage nous avons un canevas divisé en deux partie :
-- la partie de gauche sert à afficher les rectangles du jeu de données
-- la partie de droite affiche les rectangles positionnés selon la prédiction. Ceux qui sont bien placés sont en vert, ceux qui sont au mauvais endroit sont en rouge.
 
+(dans branche Master) <br/>
+Interaction avec des boutons permettant de décider le nombre de rectangles qui vont servir pour l'apprentissage et décider des données d'entrée pour la prédiction.
+- Add one learn square. Ajoute un seul rectangle à l'apprentissage du modèle
+- Add 100 learn squares. Ajoute 100 rectangles à l'apprentissage du modèle (déprécié !)
+- Ajout auto. Ajoute un rectangle à l'apprentissage du modèle par seconde.
+
+- Predict. Essaye de prédire la position d'un rectangle et l'affiche à droite. (hauteur et largueur doivent être < 400 !)
+- Predict the test. Envoie les données d'apprentissage sur la prédiction et affiche le pourcentage de réussite.
+- Predict from a file. Ne marche pas encore.
+
+(dans branche ellie) <br/>
+Une deuxième version a été faite, où cette fois un jeu de données est généré dès le début et le résultat est directement affiché (avec les rectangles des données à gauche et les rectangles prédits à droite).
 On affiche également sur la console le pourcentage de bonnes prédictions.
 
 
+
 Liens
-====
+=====
 
  [Enregistrer JSON](https://stackoverflow.com/questions/34156282/how-do-i-save-json-to-local-text-file)
+ 
+NB
+==
+Nous utilisons P5 et MDL pour l'affichage de la page et le dessins dans le canvas.
--- web/sketch.js
+++ web/sketch.js
@@ -1,76 +1,37 @@
-let dimensions = [];
-let positions = [];
+let all_squares_display = { squareCoord: [], pos: [], color: [], predictSquare: [], posPredict: [], colorPredict: [] };
+let all_squares_learn = { squareLearn: [], posLearn: [] };
 
-let all_learn_squares = { squareCoord: [], pos: [], color: [] };
-let all_predict_squares = { squareCoord: [], pos: [], color: [] };
+let buttonAutoAdd;
+let autoAjout = false;
 
-const learningRate = 0.2;
-const optimizer = 'sgd';
-let model;
+const learningRate = 0.01;
+const optimizer = tf.train.sgd(learningRate);
 
-//Là où on stocke les positions obtenues en résultat
-let res;
+let model, xs, ys;
+let history;
 
-//les tensors utilisés pour l'apprentissage
-let x_train;
-let y_train;
-
-/**
-Génère un entier aléatoire compris entre 0 et max
-*/
-function randomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-
-/**
-Génère un jeu de données
-*/
-function generateDataSet() {
-    for (var i = 0; i < 5000; i++) {
-        largeur = randomInt(400);
-        hauteur = randomInt(400);
-        dimensions.push([hauteur, largeur]);
-        if (hauteur * largeur > 30000)
-            positions[i] = [1];
-        else
-            positions[i] = [0];
-    }
-}
-
-/**
-Crée un réseau neuronal
+/*
+Création d'un premier réseau neuronal
+Couche d'entrée : 2 neurones (hauteur et largeur)
+1 couche cachée : 3 neurones
+Couche de sortie : 2 neurones ("Haut" et "Bas")
 */
 function createNeuralNetwork() {
     model = tf.sequential();
 
-    const hiddenConfig1 = {
+    const hiddenConfig = {
         inputShape: [2],
-        units: 6,
+        units: 3,
         activation: 'relu'
     };
-
-    const hiddenConfig2 = {
-        units: 6,
-        activation: 'relu'
-    };
-
-    const hiddenConfig3 = {
-        units: 2,
-        activation: 'relu'
-    };
-
     const outputConfig = {
+        inputShape: [3],
         units: 1,
-        activation: 'sigmoid'
+        //   activation: 'sigmoid'
     };
-    let hiddenLayer1 = tf.layers.dense(hiddenConfig1);
-    let hiddenLayer2 = tf.layers.dense(hiddenConfig2);
-    let hiddenLayer3 = tf.layers.dense(hiddenConfig3);
+    let hiddenLayer = tf.layers.dense(hiddenConfig);
     let outputLayer = tf.layers.dense(outputConfig);
-    model.add(hiddenLayer1);
-    model.add(hiddenLayer2);
-    //model.add(hiddenLayer3);
+    model.add(hiddenLayer);
     model.add(outputLayer);
 
     model.compile({
@@ -78,93 +39,150 @@ function createNeuralNetwork() {
         loss: 'meanSquaredError',
         lr: 0.1
     });
-
 }
 
-/**
-Crée le réseau neuronal, réalise la prédiction, génère les rectangles issus de la prédiction et affiche un pourcentage de succès
-*/
-async function predictOutput() {
-    createNeuralNetwork();
-    console.log("Neural network created");
-
-    const xs = tf.tensor2d(dimensions);
-    //xs.print();
-
-    await model.fit(x_train, y_train, { batchsize: 1, epochs: 1 });
-    ys = model.predict(xs);
-    //ys.print();
-    res = Array.from(ys.dataSync());
-    console.log("Résultat :");
-    console.log(res);
-    setPredictSquares();
-    console.log("Pourcentage de réussite : " + calcSuccessPercent() + " %");
+function download(content, fileName, contentType = "application/json") {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
 }
 
-/**
-Remplit la structure de données contenant tous les rectangles du jeu de données
-*/
-function setLearnSquares() {
-    for (var i = 0; i < dimensions.length; i++) {
-        all_learn_squares["squareCoord"].push({ l: dimensions[i][1], h: dimensions[i][0] });
-        all_learn_squares["pos"].push(positions[i]);
-        all_learn_squares["color"].push({ r: random(255), g: random(255), b: random(255) });
+// Retourne vrai si on a affaire à un grand rectangle
+// et false sinon
+function predictLH(l, h) {
+    if (h * l > 30000) {
+        return true;
     }
+    return false;
 }
 
-/**
-Remplit la structure de données contenant tous les rectangles à tracer après la prédiction
-*/
-function setPredictSquares() {
-    for (var i = 0; i < res.length; i++) {
-        all_predict_squares["squareCoord"].push({ l: dimensions[i][1], h: dimensions[i][0] });
-        all_predict_squares["pos"].push(res[i]);
+function predictTheTests() {
+    let cpt = 0;
+    let correctTest = 0;
 
-        if (isCorrect(positions[i][0], res[i])) {
-            all_predict_squares["color"].push({ r: 0, g: 255, b: 0 });
-        } else {
-            all_predict_squares["color"].push({ r: 255, g: 0, b: 0 });
+    // Parcourt tous les carré de la partie apprentisage et les prédict
+    for (i = 0; i < all_squares_learn.squareLearn.length; i += 2) {
+        let res = predictAndDisplay(all_squares_learn.squareLearn[i] * 390 + 10, all_squares_learn.squareLearn[i + 1] * 390 + 10);
+        if (res[1]) {
+            correctTest++;
         }
+        cpt++;
     }
+
+    // Affiche le % de réussite
+    select("#percentSuccess").html(parseInt((correctTest / cpt) * 10000) / 100 + "%");
+    console.warn("Correct : " + parseInt((correctTest / cpt) * 10000) / 100 + "%");
+}
+
+function predictAndDisplay(lgr, htr) {
+    // Si la hauteur et la largeur n'es pas bonne, ne rien faire et alerter
+    if (lgr > 400 || htr > 400 || lgr < 10 || htr < 10) {
+        console.error("Largeur et hauteur doivent être entre 10 et 400");
+        return;
+    }
+
+    // Predict en donnant la largeur et la hauteur normalisé
+    tensorRes = model.predict(tf.variable(tf.tensor2d([
+        [(lgr - 10) / 390, (htr - 10) / 390]
+    ], [1, 2])));
+
+
+    // The result of the prediction in an Array
+    let res = Array.from(tensorRes.dataSync());
+    // Say if the prediction correspond to the reality
+    let isCorrect = false;
+
+    // Rajoute le carré a prédire dans les carré à affiché
+    all_squares_display.predictSquare.push({ l: lgr, h: htr });
+
+    // Lui donne une couleur en fonction de si il est bien placé où nonS
+    if (res[0] > 0.5) {
+        if (predictLH(lgr, htr)) {
+            all_squares_display["colorPredict"].push({ r: 0, g: 255, b: 0 });
+            isCorrect = true;
+        } else { all_squares_display["colorPredict"].push({ r: 255, g: 0, b: 0 }); }
+
+        all_squares_display["posPredict"].push("Haut");
+    } else {
+        if (!predictLH(lgr, htr)) {
+            all_squares_display["colorPredict"].push({ r: 0, g: 255, b: 0 });
+            isCorrect = true;
+        } else { all_squares_display["colorPredict"].push({ r: 255, g: 0, b: 0 }); }
+
+        all_squares_display["posPredict"].push("Bas");
+    }
+
+    // Log et retourne le résultat
+    tensorRes.print();
+    console.log(isCorrect);
+    return [res, isCorrect];
+}
+
+function predictFromUser() {
+    let lgr = parseInt(select("#largeur").value());
+    let htr = parseInt(select("#hauteur").value());
+
+    predictAndDisplay(lgr, htr);
 }
 
 
-/**
-Prend en paramètre une position du jeu de données et la position prédite correspondante et vérifie si la prédiction est correcte ou non
-*/
-function isCorrect(pos, predict_pos) {
-    return (pos == 0 && predict_pos < 0.5) || (pos == 1 && predict_pos > 0.5);
-}
+async function addSquare() {
+    // Calcul une hauteur et une largeur random
+    largeur = int(random(10, 400));
+    hauteur = int(random(10, 400));
 
-/**
-Calcule le pourcentage de prédictions correctes
-*/
-function calcSuccessPercent() {
-    var correctPosCount = 0;
-    for (var i = 0; i < positions.length; i++) {
-        if (isCorrect(positions[i], res[i])) correctPosCount++;
+    // La met dans le tableau all_squares_display
+    all_squares_display["squareCoord"].push({ l: largeur, h: hauteur });
+    all_squares_learn.squareLearn.push((largeur - 10) / 390);
+    all_squares_learn.squareLearn.push((hauteur - 10) / 390);
+
+    // Si grand rectangle, va en haut, sinon va en bas
+    if (predictLH(hauteur, largeur)) {
+        all_squares_display["pos"].push("Haut");
+        all_squares_learn["posLearn"].push(1);
+    } else {
+        all_squares_display["pos"].push("Bas");
+        all_squares_learn["posLearn"].push(0);
     }
-    return correctPosCount * 100 / positions.length;
+    // Lui choisis une couleur random (pour affichage)
+    all_squares_display["color"].push({ r: random(255), g: random(255), b: random(255) });
+
+    xs = tf.tensor2d(all_squares_learn.squareLearn, [all_squares_display.squareCoord.length, 2]);
+    ys = tf.tensor1d(all_squares_learn.posLearn);
+
+    console.warn("Tranning !");
+    history = await model.fit(xs, ys);
 }
 
 function setup() {
-    createCanvas(1300, 800);
-    frameRate(1);
-    generateDataSet();
-    console.log("Positions initiales :");
-    console.log(positions);
-    setLearnSquares();
-    x_train = tf.tensor2d(dimensions);
-    //console.log("x_train");
-    //x_train.print();
-    y_train = tf.tensor2d(positions);
-    //console.log("y_train");
-    //y_train.print();
-    predictOutput();
+    // 2 rectangle de 600*800 avec un gap de 100 entre les 2
+    let canvas = createCanvas(1300, 800);
+    canvas.parent('sketch-holder');
+    // frameRate(1);  // Change les fps à 1 images par secondes
 
+    let button = select("#Add1");
+    button.mousePressed(addSquare);
+
+    button = select("#Add100");
+    button.mousePressed(function() { for (i = 0; i < 100; i++) { addSquare(); } });
+
+    button = select("#predict");
+    button.mousePressed(predictFromUser);
+
+    button = select("#predictTests");
+    button.mousePressed(predictTheTests);
+
+    buttonAutoAdd = select("#AddFrame");
+    buttonAutoAdd.mousePressed(function() { autoAjout = !autoAjout; if (autoAjout) { buttonAutoAdd.elt.innerText = "Ajout auto : activé"; } else { buttonAutoAdd.elt.innerText = "Ajout auto : désactivé"; } });
+
+    createNeuralNetwork();
 }
 
-
+/*
+ * Fonction appelé par p5 à chaque frame
+ */
 function draw() {
     // Arrière plan
     background(255);
@@ -181,31 +199,37 @@ function draw() {
     stroke('#222222');
     rect(600, 0, 100, 800);
 
-    // Draw all learning rectangles
-    for (i = 0; i < all_learn_squares["squareCoord"].length; i++) {
+    // Déssine chaque rectangle d'entrainement de all_squares_display
+    for (i = 0; i < all_squares_display["squareCoord"].length; i++) {
         xGap = (i % 50) * 5;
         yGap = (int(i / 50) * 20) + 20;
 
-        if (all_learn_squares.pos[i] == "0") {
+        if (all_squares_display.pos[i] == "Bas") {
             yGap += 400;
         }
-        fill(all_learn_squares.color[i].r, all_learn_squares.color[i].g, all_learn_squares.color[i].b);
-        rect(xGap, yGap, all_learn_squares.squareCoord[i].l, all_learn_squares.squareCoord[i].h);
+
+
+        fill(all_squares_display.color[i].r, all_squares_display.color[i].g, all_squares_display.color[i].b);
+        rect(xGap, yGap, all_squares_display.squareCoord[i].l, all_squares_display.squareCoord[i].h);
     }
 
-    // Draw all the predicted rectangles
-    for (i = 0; i < res.length; i++) {
+    strokeWeight(1);
+    // Déssine les rectangles prédits
+    for (i = 0; i < all_squares_display.predictSquare.length; i++) {
         xGap = 700 + (i % 50) * 5;
         yGap = (int(i / 50) * 20) + 20;
 
-        if (all_predict_squares.pos[i] < 0.5) {
+        if (all_squares_display.posPredict[i] == "Bas") {
             yGap += 400;
         }
-        fill(all_predict_squares.color[i].r, all_predict_squares.color[i].g, all_predict_squares.color[i].b);
-        rect(xGap, yGap, all_predict_squares.squareCoord[i].l, all_predict_squares.squareCoord[i].h);
+
+
+        fill(all_squares_display.colorPredict[i].r, all_squares_display.colorPredict[i].g, all_squares_display.colorPredict[i].b);
+        rect(xGap, yGap, all_squares_display.predictSquare[i].l, all_squares_display.predictSquare[i].h);
     }
 
-
-
-
+    // Si activé par le bouton, rajoute un nouveau rectangle d'entrainement
+    if (autoAjout) {
+        addSquare();
+    }
 }
